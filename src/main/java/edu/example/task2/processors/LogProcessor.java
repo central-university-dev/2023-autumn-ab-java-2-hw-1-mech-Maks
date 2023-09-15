12a76bde3e99e7904a99bab94adc196552bfde16
Artem Lysenko <artemlysenko@niuitmo.ru>
2023-09-15 14:57:33 +0300

    solved tasks

---
+++ src/main/java/edu/example/task2/processors/LogProcessor.java
@@ -0,0 +1,11 @@
+package edu.example.task2.processors;
+
+import edu.example.task2.Person;
+import edu.example.task2.PersonProcessor;
+
+public class LogProcessor implements PersonProcessor {
+    @Override
+    public void process(Person person) {
+        System.out.println("changed: mom - " + person.getMother() + "; dad - " + person.getFather());
+    }
+}
---
+++ src/main/java/edu/example/task3/Task3.java
@@ -0,0 +1,34 @@
+package edu.example.task3;
+
+import lombok.Getter;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class Task3 {
+    public static void main(String[] args) {
+        Variable variable = new Variable();
+        Runnable runnable = variable::inc;
+
+        ExecutorService service = Executors.newFixedThreadPool(3);
+
+        for (int i = 0; i < 3; i++) {
+            service.execute(runnable);
+        }
+
+        service.shutdown();
+        while (!service.isTerminated()) {}
+
+        System.out.println(variable.getX());
+    }
+}
+
+@Getter
+class Variable {
+    private int x = 1;
+    public synchronized void inc() {
+        for (int i = 0; i < 10_000; i++) {
+            x += 1;
+        }
+    }
+}
