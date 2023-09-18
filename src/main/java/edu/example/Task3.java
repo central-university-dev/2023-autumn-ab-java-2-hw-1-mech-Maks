8087ae493c6fcb97f30e5d3ea9e65fd4bb9b6a7c
sejapoe <bekaldiev2015@gmail.com>
2023-09-18 19:45:09 +0300

    fix task 3

--- src/main/java/edu/example/Task3.java
+++ src/main/java/edu/example/Task3.java
@@ -1,6 +1,7 @@
 package edu.example;
 
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.atomic.AtomicInteger;
 
 
@@ -10,17 +11,23 @@ public class Task3 {
     public static void main(String[] args) {
 
         CompletableFuture<Void> futures = CompletableFuture.allOf(
-                createFuture(),
-                createFuture(),
-                createFuture()
+                incrementXFuture(),
+                incrementXFuture(),
+                incrementXFuture()
         );
         futures.thenApply(unused -> {
             System.out.println(x.get());
             return null;
         });
+
+        try {
+            futures.get();
+        } catch (InterruptedException | ExecutionException e) {
+            System.out.println(e);
+        }
     }
 
-    private static CompletableFuture<Void> createFuture() {
+    private static CompletableFuture<Void> incrementXFuture() {
         return CompletableFuture.runAsync(() -> {
             for (int i = 0; i < 10000; i++) {
                 x.incrementAndGet();
