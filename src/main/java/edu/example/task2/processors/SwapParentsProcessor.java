c8f08619d5f2f241ed287c0e5ce4561df397eb2a
Ramal <ramalguseynov03@gmail.com>
2023-09-18 07:45:32 +0300

    update SwapPqrentsProcessor

--- src/main/java/edu/example/task2/processors/SwapParentsProcessor.java
+++ src/main/java/edu/example/task2/processors/SwapParentsProcessor.java
@@ -1,13 +1,16 @@
 package edu.example.task2.processors;
 
 import edu.example.task2.Person;
+
 import java.util.function.Consumer;
 
 public class SwapParentsProcessor implements Consumer<Person> {
     @Override
     public void accept(Person person) {
-        Person temp = person.getMother();
-        person.setMother(person.getFather());
-        person.setFather(temp);
+        if (person != null) {
+            Person temp = person.getMother();
+            person.setMother(person.getFather());
+            person.setFather(temp);
+        }
     }
 }
