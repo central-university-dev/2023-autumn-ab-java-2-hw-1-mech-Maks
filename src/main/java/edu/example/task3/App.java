437df2c87d36654ed26a9954f87170874d4ed518
bridges123 <i.s.andreevmail.rut@gmail.com>
2023-09-19 21:41:07 +0300

    update task3

--- src/main/java/edu/example/task3/App.java
+++ src/main/java/edu/example/task3/App.java
@@ -1,25 +1,32 @@
 package edu.example.task3;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class App {
     public static void main(String[] args) {
-        AtomicInteger k = new AtomicInteger(1);
-        List<CountThread> threads = new ArrayList<>();
-        for (int i = 0; i < 3; i++) {
-            CountThread thread = new CountThread(k, 10000);
-            threads.add(thread);
-            thread.start();
-        }
-        for (CountThread thread : threads) {
-            try {
-                thread.join();
-            } catch (InterruptedException e) {
-                Thread.currentThread().interrupt();
-                e.printStackTrace();
+        AtomicInteger counter = new AtomicInteger(1);
+        ExecutorService service = Executors.newFixedThreadPool(3);
+        try {
+            List<Future<?>> futures = Stream.of(
+                    new IncrementRunnable(counter),
+                    new IncrementRunnable(counter),
+                    new IncrementRunnable(counter)
+            ).map(service::submit).collect(Collectors.toList());
+            for (Future<?> f : futures) {
+                f.get();
             }
+            System.out.println("counter: " + counter.get());
+        } catch (InterruptedException | ExecutionException e) {
+            throw new RuntimeException(e);
+        } finally {
+            service.shutdown();
         }
     }
 }
