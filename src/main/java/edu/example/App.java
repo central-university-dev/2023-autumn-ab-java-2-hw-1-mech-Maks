e5277cb896dff191a102517732b51beab1e9790f
Alexander Avershin <55056382+asavershin@users.noreply.github.com>
2023-09-19 13:53:00 +0300

    app to App

--- src/main/java/edu/example/App.java
+++ src/main/java/edu/example/App.java
@@ -11,7 +11,7 @@ import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 
-public class app {
+public class App {
     public static void main(String[] args) {
 
         PersonChainProcessor chainProcessor = new PersonChainProcessor();
