7556e452e07aee2913be2a6ba65c442c1ff61ba3
MinusD <lukovnikov50@mail.ru>
2023-09-20 14:52:02 +0300

    Исправлено задание с несколькими потоками

--- src/main/java/edu/example/task3/App.java
+++ src/main/java/edu/example/task3/App.java
@@ -1,44 +1,40 @@
 package edu.example.task3;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicInteger;
+
 /**
  * Перенёс из отдельного репозитория
  */
 public class App {
-    private static int counter = 1;
 
-    private static synchronized void incrementCounter() {
-        counter++;
-    }
+    private static AtomicInteger counter = new AtomicInteger(1);
+
 
     public static void main(String[] args) {
-        var th1 = new Thread(() -> {
-            for (int i = 0; i < 10000; i++) {
-                incrementCounter();
-            }
-        });
-        var th2 = new Thread(() -> {
-            for (int i = 0; i < 10000; i++) {
-                incrementCounter();
-            }
-        });
-        var th3 = new Thread(() -> {
-            for (int i = 0; i < 10000; i++) {
-                incrementCounter();
-            }
-        });
-        th1.start();
-        th2.start();
-        th3.start();
+        ExecutorService executorService = Executors.newFixedThreadPool(3);
 
-        // Ждём пока потоки закончатся
+        for (int i = 0; i < 3; i++) {
+            executorService.submit(
+                    () -> {
+                        for (int j = 0; j < 10000; j++) {
+                            // Надеюсь правильно понял, что такое кол-во кода лучше оставить в лямбде
+                            counter.incrementAndGet();
+                        }
+                    }
+            );
+        }
+
+        executorService.shutdown();
+
+        // Ждём пока потоки завершатся
         try {
-            th1.join();
-            th2.join();
-            th3.join();
-        } catch (Exception e) {
+            executorService.awaitTermination(Long.MAX_VALUE, java.util.concurrent.TimeUnit.NANOSECONDS);
+        } catch (InterruptedException e) {
             System.out.println("Threads Error :(");
         }
 
-        System.out.println("Counter value: " + counter);
+        System.out.println(counter);
     }
 }
